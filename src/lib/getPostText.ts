ef7dc7aebff09f4dc1a7e2373b169a57b6e28a0f
yuta-hayashi <dev.hyuta@gmail.com>
2023-08-16 13:04:33 +0900

    Add get zenn trending feed function.

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,3 +1,9 @@
 export default async function getPostText() {
-  return 'Now reading👀 \nhttps://github.com/bluesky-social/atproto'
+  const res = await fetch('https://zenn-api.vercel.app/api/trendTech')
+    .then((res) => res.json())
+    .catch((err) => {
+      throw new Error(err)
+    })
+  const article = res[0]
+  return `${article.emoji} ${article.title}\nby ${article.user.name}\nhttps://zenn.dev${article.path}`
 }
